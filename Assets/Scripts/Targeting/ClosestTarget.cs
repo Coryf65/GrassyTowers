3e853c2e4d5cacb4d72191428cc001ca8dfc56c7
Cory <coryf65@gmail.com>
2020-12-19 17:12:57 -0600

    Targeting System
    Creating a Targeting System
    
    closest Target being worked on

---
+++ Assets/Scripts/Targeting/ClosestTarget.cs
@@ -0,0 +1,38 @@
+﻿using Cory.TowerGame.Enemies;
+using Cory.TowerGame.Towers;
+using UnityEngine;
+
+namespace Cory.TowerGame.Targeting
+{
+    public class ClosestTarget : TargetingSystem
+    {
+        // inherited Target, with our implementation
+        protected override void FindTarget()
+        {
+            // we are telling this the amount of mem to use
+            // does a Sphere check at the range and only colliding with our layer we set
+            int colliderCount = Physics.OverlapSphereNonAlloc(transform.position, towerData.Range, colliderBuffer, layerMask);
+
+            Enemy closestEnemy = null;
+            float closestDistance = Mathf.Infinity;
+
+            for (int i = 0; i < colliderCount; i++)
+            {
+                // checking the distance between towers and enemies
+                float distanceSquared = (colliderBuffer[i].transform.position - transform.position).sqrMagnitude;
+
+                if (distanceSquared < closestDistance * closestDistance)
+                {
+                    // make sure this is an enemy, if so pass out the Enemy Component
+                    if (colliderBuffer[i].TryGetComponent<Enemy>(out var enemy))
+                    {
+                        closestDistance = distanceSquared;
+                        closestEnemy = enemy;
+                    }
+                }
+            }
+
+            Target = closestEnemy;
+        }
+    }
+}
---
+++ Assets/Scripts/Targeting/TargetingSystem.cs
@@ -0,0 +1,49 @@
+﻿using Cory.TowerGame.Enemies;
+using Cory.TowerGame.Towers;
+using UnityEngine;
+
+namespace Cory.TowerGame.Targeting
+{
+    public abstract class TargetingSystem : MonoBehaviour
+    {
+        // cory
+        [Header("Tower Connections")]
+        [SerializeField] protected TowerData towerData = null;        
+        [Space][Header("Tower Settings")]        
+        [Tooltip("How often the Tower checks for enemies to shoot.")]
+        [SerializeField] private float checkRate = 0.5f;
+        [Tooltip("Which Layer this tower will look for an Enemy to shoot.")]
+        [SerializeField] protected LayerMask layerMask = new LayerMask();
+
+        private float timer;
+
+        // allocating a set of memory and only use up to this much
+        // setting the max amount of enemies to check for from a turret
+        // abouts how many enemies that are on the screen at one time
+        protected readonly Collider[] colliderBuffer = new Collider[50];
+
+        public Enemy Target { get; protected set; }
+
+        public void Update()
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0f)
+            {
+                timer = checkRate;
+
+                FindTarget();
+            }
+
+            if (Target = null) return;
+
+            // aim at the target
+            transform.LookAt(Target.transform);
+        }
+
+        // cannot make an instance of this script but any class inheriting this needs to implement
+        // the abstract functions
+        protected abstract void FindTarget();
+
+    }
+}
