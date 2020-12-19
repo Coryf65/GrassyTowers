fa02f03d75e22804fa04456fda98486325296e19
Cory <coryf65@gmail.com>
2020-12-18 19:53:29 -0600

    Enemy Nodes
    Creating a path for enemies to travel
    follows a node trail

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,51 @@ namespace Cory.TowerGame.Enemies
     {
         // reference our Scriptable Object
         [SerializeField] private EnemyData enemyData = null;
+        [SerializeField] private Node startingNode = null;
 
+        private Node targetNode;
+        private Vector3 currentDirection;
+
+        private const float MinDistance = 0.1f;
+
+
+        public void Start()
+        {
+            targetNode = startingNode;
+        }
+
+        public void Update()
+        {
+            if (targetNode == null) { return; }
+
+            MoveToTarget();
+
+        }
+
+        private void MoveToTarget()
+        {
+            // capping to our frame rate
+            transform.Translate(currentDirection * enemyData.MovementSpeed * Time.deltaTime, Space.World);
+
+            if ((transform.position - targetNode.transform.position).sqrMagnitude < MinDistance * MinDistance)
+            {
+                SetNode(targetNode.NextNode);
+            }
+        }
+
+        private void SetNode(Node nextNode)
+        {
+            targetNode = nextNode;
+
+            if (targetNode == null) { return; }
+            // rotate
+            currentDirection = (targetNode.transform.position - transform.position).normalized;
+
+            if (currentDirection != Vector3.zero)
+            {
+                // face this direction
+                transform.rotation = Quaternion.LookRotation(currentDirection);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Enemies/Node.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cory.TowerGame.Enemies
+{
+    public class Node : MonoBehaviour
+    {
+        [SerializeField] private Node[] nodes = new Node[0];
+
+        private int nextNodeIndex;
+
+        // get a node
+        public Node NextNode
+        {
+            get 
+            {
+                // check if we are at the last node
+                if (nodes.Length == 0) return null;
+
+                Node nextNode = nodes[nextNodeIndex];
+
+                // the next one after we try to increase
+                // if their is another one go there
+                if (nextNodeIndex + 1 != nodes.Length)
+                {
+                    nextNodeIndex++;
+                } else
+                {
+                    // go back to the start
+                    nextNodeIndex = 0;
+                }
+
+                return nextNode;    
+            }
+        }
+
+        #region Debug
+        /// <summary>
+        /// Drawing a color wire sphere in the Editor
+        /// </summary>
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            // offset off the ground
+            Vector3 myPosition = transform.position + new Vector3(0f, 0.5f, 0f);
+            Gizmos.DrawWireSphere(myPosition, 0.5f);
+
+            foreach (var node in nodes)
+            {
+                // draw a line to connect the nodes
+                Gizmos.DrawLine(myPosition, node.transform.position + new Vector3(0f, 0.5f, 0f));
+            }
+
+        }
+        #endregion 
+    }
+}
