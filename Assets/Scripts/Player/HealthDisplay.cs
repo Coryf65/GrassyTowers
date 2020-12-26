465cf11b8df922971770420d47e4f85a0ef16f0e
Cory <coryf65@gmail.com>
2020-12-26 14:23:33 -0600

    UI health System
    Player now has health and enemies can damage the player

---
+++ Assets/Scripts/Player/HealthDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+namespace Cory.TowerGame.Player
+{
+    public class HealthDisplay : MonoBehaviour
+    {
+        [SerializeField] private PlayerHealthSystem healthSystem = null;
+        [SerializeField] private TMP_Text healthText = null;
+
+        private void OnEnable()
+        {
+            // if this gets called before the other script
+            // then we read the data
+            HandleHealthChanged(healthSystem.Health);
+
+            healthSystem.OnHealthChanged += HandleHealthChanged;
+        }
+
+        private void OnDisable()
+        {
+            healthSystem.OnHealthChanged -= HandleHealthChanged;
+        }
+
+        private void HandleHealthChanged(int health)
+        {
+            healthText.text = health.ToString();
+        }
+
+    }
+}
--- Assets/Scripts/Player/PlayerHealthSystem.cs
+++ Assets/Scripts/Player/PlayerHealthSystem.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using Cory.TowerGame.Enemies;
+using Cory.TowerGame.Waves;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +11,29 @@ namespace Cory.TowerGame.Player
     {
         [Header("Destroy after duration elapsed")]
         [Tooltip("How long in seconds before this Gameobject will be destroyed.")]
-        [SerializeField] private int totalHealth = 10;
+        [SerializeField] private int health = 10;
+
+        public event Action<int> OnHealthChanged;
+        public static Action OnGameOver;
+
+        // public getter
+        public int Health => health;
+
+        // Subsribing to the event
+        private void OnEnable() => WaveDestination.OnEnemyReachedEnd += HandleEnemyReachedEnd;
+        // Unsubscribing to the event
+        private void OnDisable() => WaveDestination.OnEnemyReachedEnd -= HandleEnemyReachedEnd;
+
+        private void HandleEnemyReachedEnd(EnemyData enemyData)
+        {
+            // deal damage to the player
+            health = Mathf.Max(health - enemyData.Damage);
+
+            OnHealthChanged?.Invoke(health);
+
+            if (health != 0) { return; }
+            // if health = 0 Game Over!
+            OnGameOver?.Invoke();
+        }
     }
 }
